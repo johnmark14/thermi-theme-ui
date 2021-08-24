bd0b884a8011de902f0361899536d4d66d97a56e
johnmark14 <johmarkgabrentina@gmail.com>
2021-08-25 01:32:12 +0800

    fix side nav animation

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+const App = {
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isOpen = !this.isOpen
+        }
+    },
+}
+
+Vue.createApp(App).mount('#app');
