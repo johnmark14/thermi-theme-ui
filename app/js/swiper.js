cae0563b27e73f1dac8d222e0b280907975b2e2c
johnmark14 <johmarkgabrentina@gmail.com>
2021-08-28 22:36:59 +0800

    added product carousel

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -12,3 +12,29 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     }
 });
+
+var swiperProduct = new Swiper(".carProducts", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+        639: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        375: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
